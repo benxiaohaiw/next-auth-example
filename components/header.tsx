d4ef34a1a7aec1b1d57ef532a68dba5578ba4af6
benxiaohaiw <67253141+benxiaohaiw@users.noreply.github.com>
2022-10-19 13:26:38 +0800

    对登录流程做补充说明

--- components/header.tsx
+++ components/header.tsx
@@ -30,7 +30,16 @@ export default function Header() {
                 className={styles.buttonPrimary}
                 onClick={(e) => {
                   e.preventDefault()
-                  signIn()
+                  signIn() // 未登录点击登录
+                  // 此函数是next-auth/react中提供的方法
+                  // 当前没有传入provider参数，所以它里面的逻辑是直接location.href = /api/auth/signin?callbackUrl=xxx 注意它是get请求
+                  // 那么浏览器就会访问这个链接 - 这个例子中就会收到这样的请求，在next-auth内部就会返回默认的登录页面啦 ~
+                  // 在登录页面中点击github登录就会发送/api/auth/signin/github?xxx，只不过它是post请求，和上面的是不一样的逻辑的
+                  // 这个返回的是302响应 location为github三方登录的地址，之后浏览器就会自动访问这个location地址
+                  // 注意这个location地址中的redirect_url参数是传给github的，它是登录成功后github回调的地址
+                  // 这个地址就是/api/auth/callback/github?xxx，在这个里面的逻辑就是根据session的策略来进行存储session啦
+                  // 然后返回cookie和302响应 location为callbackUrl，这个callbackUrl可以通过req的body中的或者是req的query的callbackUrl参数来去决定设置的
+                  // 然后浏览器就访问这个设置的callbackUrl啦 ~
                 }}
               >
                 Sign in
